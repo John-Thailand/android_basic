5a269d60ac50a1dcea34021f7c91b7e493bc02df
John-Thailand <naoki.honda@mirrorfit.jp>
2024-10-10 18:47:32 +0900

    チェックボックスの実装

--- app/src/main/java/to/msn/wings/dialogargs/MyDialogFragment.kt
+++ app/src/main/java/to/msn/wings/dialogargs/MyDialogFragment.kt
@@ -8,27 +8,29 @@ import androidx.fragment.app.DialogFragment
 
 class MyDialogFragment : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        var selected = 0
-        val items = arrayOf("A型", "B型", "O型", "AB型")
+        val items = arrayOf("電車", "バス", "徒歩", "マイカー", "自転車", "その他")
+        val selected = booleanArrayOf(true, true, true, false, false, false)
+
         val dialog = activity?.let {
             AlertDialog.Builder(it).apply {
-                setTitle("血液型")
+                setTitle("通勤手段")
                 setIcon(R.drawable.wings)
-                // 単一選択型のリストを生成
-                setSingleChoiceItems(items, selected) { dialog, which ->
-                    selected = which
+                setMultiChoiceItems(items, selected) { dialog, which, isChecked ->
+                    selected[which] = isChecked
                 }
-                // OKボタンを生成
                 setPositiveButton("OK") { dialog, which ->
+                    val msg = mutableListOf<String>()
+                    selected.forEachIndexed { index, e ->
+                        if (e) msg.add(items[index])
+                    }
                     Toast.makeText(
                         activity,
-                        "「${items[selected]}」が選択されました",
+                        "「${msg.joinToString()}}」が選択されました。",
                         Toast.LENGTH_SHORT
                     ).show()
                 }
             }.create()
         }
-        // ダイアログを表示
         return dialog ?: throw IllegalStateException("Activity is null.")
     }
 }
