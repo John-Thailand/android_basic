e6f935c4196ce88c51e2faa4de49aab9d40b97b6
John-Thailand <naoki.honda@mirrorfit.jp>
2024-10-29 18:19:34 +0900

    画像を描画

--- app/src/main/java/to/msn/wings/dialogargs/SimpleView.kt
+++ app/src/main/java/to/msn/wings/dialogargs/SimpleView.kt
@@ -1,6 +1,7 @@
 package to.msn.wings.dialogargs
 
 import android.content.Context
+import android.graphics.BitmapFactory
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
@@ -10,22 +11,12 @@ import android.util.AttributeSet
 import android.view.View
 
 class SimpleView(context: Context, attrs: AttributeSet) : View(context, attrs) {
-    private val p = Paint().apply {
-        color = Color.CYAN
-        strokeWidth = 5f
-        typeface = Typeface.SERIF
-        textSize = 50f
-        textAlign = Paint.Align.CENTER
-        textScaleX = 1.5f
-        textSkewX = -0.5f
-    }
+    private val bmp = BitmapFactory.decodeResource(resources, R.drawable.wings)
+    private val p = Paint()
 
-    // 描画のコア
-    // Androidがビューを表示/更新する際に呼び出すメソッド
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-
         canvas?.drawColor(Color.WHITE)
-        canvas?.drawText("WINGSプロジェクト", 400f, 300f, p)
+        canvas?.drawBitmap(bmp, 0f, 10f, p)
     }
 }
