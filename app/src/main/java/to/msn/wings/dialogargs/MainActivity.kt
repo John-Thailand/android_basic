2f6d76f26fc7dc4cf4758f09cb003b58692464b6
John-Thailand <naoki.honda@mirrorfit.jp>
2024-11-03 15:13:11 +0900

    呼び出し先のアクティビティから結果を受け取る

--- app/src/main/java/to/msn/wings/dialogargs/MainActivity.kt
+++ app/src/main/java/to/msn/wings/dialogargs/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -12,15 +14,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // インテントを生成&データをセット
+        // 結果を受け取った時の処理を定義
+        val startForResult = registerForActivityResult(
+            StartActivityForResult()) { result ->
+            // 結果コードをチェック
+            if (result?.resultCode == RESULT_OK) {
+                result.data?.let {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "こんにちは、${it.getStringExtra("txtName")}さん！",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
+
         val btn = findViewById<Button>(R.id.btnSend)
         btn.setOnClickListener {
-            val txtName = findViewById<EditText>(R.id.txtName)
-            startActivity(
-                Intent(this, SubActivity::class.java).apply {
-                    putExtra("txtName", txtName.text.toString())
-                }
-            )
+            // SubActivityを結果を戻してもらう前提で呼び出し
+            startForResult.launch(Intent(this, SubActivity::class.java))
         }
     }
 }
--- app/src/main/java/to/msn/wings/dialogargs/SubActivity.kt
+++ app/src/main/java/to/msn/wings/dialogargs/SubActivity.kt
@@ -1,8 +1,10 @@
 package to.msn.wings.dialogargs
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -11,18 +13,18 @@ class SubActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sub)
 
-        // インテントを取得&トーストに反映
-        val txtName = intent.getStringExtra("txtName")
-        Toast.makeText(
-            this, "こんにちは、${txtName}さん！",
-            Toast.LENGTH_SHORT
-        ).show()
-
-        // ボタンクリック時に呼び出されるイベントリスナー
         val btn = findViewById<Button>(R.id.btnBack)
         btn.setOnClickListener {
-            // SubActivityを終了
-            finish()
+            val txtName = findViewById<EditText>(R.id.txtName)
+            // 結果情報を設定し、現在のアクティビティを終了
+            setResult(
+                RESULT_OK,
+                Intent().apply {
+                    // インテントにテキストボックスからの入力値をセット
+                    putExtra("txtName", txtName.text.toString())
+                }
+            )
         }
+        finish()
     }
 }
