b7680b140458430aa2ff7c9b6863ab54f9f8f22d
John-Thailand <naoki.honda@mirrorfit.jp>
2024-11-03 12:47:53 +0900

    プログラムから適用

--- app/src/main/java/to/msn/wings/dialogargs/MainActivity.kt
+++ app/src/main/java/to/msn/wings/dialogargs/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_MaterialComponents)
         setContentView(R.layout.activity_main)
     }
 }
