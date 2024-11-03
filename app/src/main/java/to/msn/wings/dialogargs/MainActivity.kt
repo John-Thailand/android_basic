b29137564239b181856c1629c1e33c7981debf65
John-Thailand <naoki.honda@mirrorfit.jp>
2024-11-03 15:21:20 +0900

    暗黙的インテントの実装

--- app/src/main/java/to/msn/wings/dialogargs/MainActivity.kt
+++ app/src/main/java/to/msn/wings/dialogargs/MainActivity.kt
@@ -1,6 +1,7 @@
 package to.msn.wings.dialogargs
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
@@ -14,25 +15,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // 結果を受け取った時の処理を定義
-        val startForResult = registerForActivityResult(
-            StartActivityForResult()) { result ->
-            // 結果コードをチェック
-            if (result?.resultCode == RESULT_OK) {
-                result.data?.let {
-                    Toast.makeText(
-                        this@MainActivity,
-                        "こんにちは、${it.getStringExtra("txtName")}さん！",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-            }
-        }
-
         val btn = findViewById<Button>(R.id.btnSend)
         btn.setOnClickListener {
-            // SubActivityを結果を戻してもらう前提で呼び出し
-            startForResult.launch(Intent(this, SubActivity::class.java))
+            val txtKeywd = findViewById<EditText>(R.id.txtKeywd)
+            // インテントを作成してアクティビティを起動
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse(txtKeywd.text.toString())
+                )
+            )
         }
     }
 }
