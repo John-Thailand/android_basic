81d58cd4fa0ecd23d2367e95d51ce6af9121bf9f
John-Thailand <naoki.honda@mirrorfit.jp>
2024-11-03 17:32:47 +0900

    ページタイトルをツールバーに反映させる

--- app/src/main/java/to/msn/wings/dialogargs/MainActivity.kt
+++ app/src/main/java/to/msn/wings/dialogargs/MainActivity.kt
@@ -1,32 +1,22 @@
 package to.msn.wings.dialogargs
 
-import android.app.SearchManager
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
+import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val btn = findViewById<Button>(R.id.btnSend)
-//        btn.setOnClickListener {
-//            val txtName = findViewById<EditText>(R.id.txtName)
-//            // インテントを作成してアクティビティを起動
-//            startActivity(
-//                // ACTION_SENDアクションでテキストボックスの値を送信
-//                Intent(Intent.ACTION_SEND).apply {
-//                    type = "text/plain"
-//                    putExtra(Intent.EXTRA_TEXT, txtName.text.toString())
-//                }
-//            )
-//        }
+        // ツールバーとの紐付け
+        val navController = (supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment).navController
+        findViewById<Toolbar>(R.id.toolbar).setupWithNavController(
+            navController,
+            AppBarConfiguration(navController.graph)
+        )
     }
 }
